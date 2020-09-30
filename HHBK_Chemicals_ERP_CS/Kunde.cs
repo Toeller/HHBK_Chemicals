8e87fabcde71990810261ec96b7d1d460949963c
Töller HHBK on_premise <toeller@hhbk.de>
2020-09-30 15:03:52 +0200

    CS Klasse Kunde ergänzt

---
+++ HHBK_Chemicals_ERP_CS/Kunde.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class Kunde
+    {
+        private int kundennummer;
+        private string name;
+        private string vorname;
+        private string strasse;
+        private string hausnummer;
+        private string ort;
+        private int postleitzahl;
+        private string emailadresse;
+
+        public int Kundennummer
+        {
+            get {
+                return kundennummer;
+            }
+            
+        }
+
+        public int Postleitzahl { 
+            get => postleitzahl;
+            set
+            { 
+                if(value >= 1000 && value <= 99999)
+                    postleitzahl = value;
+            }
+        }
+    }
+}
