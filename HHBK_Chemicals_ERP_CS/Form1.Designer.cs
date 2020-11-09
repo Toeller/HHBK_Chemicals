e866cf157da21a41ab8712b906873466954d9c91
Nuerenberg.Christian <Nuerenberg.Christian@E211-18-17>
2020-11-09 12:12:55 +0100

    label ergänzt by chistian

--- HHBK_Chemicals_ERP_CS/Form1.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(116, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.Size = new System.Drawing.Size(165, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "TEEEEEEEEEEEEEEST";
+            this.label1.Text = "TEEEEEEEEEEEEEEST by chris";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // FormChris
