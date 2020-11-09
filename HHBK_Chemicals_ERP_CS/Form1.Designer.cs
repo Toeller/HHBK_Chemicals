b9be728b23cad4fdcedeb6e04d08aab0a8a148e7
Edward Danilson <edward.danilson@gmx.de>
2020-11-09 12:25:38 +0100

    Edward D

--- HHBK_Chemicals_ERP_CS/Form1.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.JustALabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // JustALabel
+            // 
+            this.JustALabel.AutoSize = true;
+            this.JustALabel.Location = new System.Drawing.Point(227, 157);
+            this.JustALabel.Name = "JustALabel";
+            this.JustALabel.Size = new System.Drawing.Size(87, 13);
+            this.JustALabel.TabIndex = 0;
+            this.JustALabel.Text = "Edward Danilson";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.JustALabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label JustALabel;
     }
 }
 
