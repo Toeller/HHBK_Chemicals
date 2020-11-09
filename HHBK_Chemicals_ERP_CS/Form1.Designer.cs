c0cce6655f51ccb21eb6ee4d882d0655434fc905
nikou.arvin <nikou.arvin@E211-18-13>
2020-11-09 12:09:32 +0100

    Label hinzugefügt

--- HHBK_Chemicals_ERP_CS/Form1.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1Arvin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1Arvin
+            // 
+            this.label1Arvin.AutoSize = true;
+            this.label1Arvin.Location = new System.Drawing.Point(331, 62);
+            this.label1Arvin.Name = "label1Arvin";
+            this.label1Arvin.Size = new System.Drawing.Size(31, 13);
+            this.label1Arvin.TabIndex = 0;
+            this.label1Arvin.Text = "Arvin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1Arvin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1Arvin;
     }
 }
 
