7cecfbfbfcc6b84650fd37dc386ca543393a1b80
Gozyuman.Mert <mert-goezy@hotmail.com>
2020-11-11 14:07:57 +0100

    Mert Form

---
+++ HHBK_Chemicals_ERP_CS/Mert Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    public partial class Mert_Form : Form
+    {
+        public Mert_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
