b02aef5ce3a195011b7841cebbf1fda2c13e62cb
TurboAlman <juliankania02@gmail.com>
2020-11-11 14:39:26 +0100

    Ergänzt

--- HHBK_Chemicals_ERP_CS/Form1.cs
+++ HHBK_Chemicals_ERP_CS/Form1.cs
@@ -16,5 +16,10 @@ namespace HHBK_Chemicals_ERP_CS
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
