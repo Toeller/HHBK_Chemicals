46cdd5c63b15419dde80db4741440af7a3ebcbd4
Aaron089 <aaron@dieschneiders.com>
2020-11-11 14:05:31 +0100

    Aarons_Form eingefügt

---
+++ HHBK_Chemicals_ERP_CS/Aarons_Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    public partial class Aarons_Form : Form
+    {
+        public Aarons_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Aarons_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
