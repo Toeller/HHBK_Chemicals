8e9c968c4db502d549b1536636fc737e5d6d78a9
Jonas Kersten <jonas.f.kersten@outlook.de>
2020-11-12 10:10:46 +0100

    Neues Fenster

---
+++ HHBK_Chemicals_ERP_CS/Form_Kersten.Jonas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    public partial class Form_Kersten: Form
+    {
+        public Form_Kersten()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form_Kersten
+            // 
+            this.ClientSize = new System.Drawing.Size(694, 261);
+            this.Name = "Form_Kersten";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
