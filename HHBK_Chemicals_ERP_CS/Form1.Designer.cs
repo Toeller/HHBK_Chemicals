6ebb51209468a63e43827920369223008ea6c10a
Noah Laptop <thecavmc@gmail.com>
2020-11-16 11:52:24 +0100

    Added Label - Noah

--- HHBK_Chemicals_ERP_CS/Form1.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Form1.Designer.cs
@@ -29,31 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.labelNoah = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-
-            this.label1.Location = new System.Drawing.Point(259, 181);
+            this.label1.Location = new System.Drawing.Point(164, 87);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Azizcan";
-
-            this.label1.Location = new System.Drawing.Point(82, 45);
-            this.label1.Name = "label2";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(66, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Töller";
-
+            // 
+            // labelNoah
+            // 
+            this.labelNoah.AutoSize = true;
+            this.labelNoah.Location = new System.Drawing.Point(335, 87);
+            this.labelNoah.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.labelNoah.Name = "labelNoah";
+            this.labelNoah.Size = new System.Drawing.Size(63, 25);
+            this.labelNoah.TabIndex = 1;
+            this.labelNoah.Text = "Noah";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.labelNoah);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -64,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelNoah;
     }
 }
 
