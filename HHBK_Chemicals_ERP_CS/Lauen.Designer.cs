22c081cc3eda190d0459197c308414fe3675701d
lauen2001 <rokki15@hotmail.de>
2020-11-18 14:29:38 +0100

    Added From - Lauen

---
+++ HHBK_Chemicals_ERP_CS/Lauen.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace HHBK_Chemicals_ERP_CS
+{
+    partial class Lauen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.labelLauen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLauen
+            // 
+            this.labelLauen.AutoSize = true;
+            this.labelLauen.Location = new System.Drawing.Point(424, 157);
+            this.labelLauen.Name = "labelLauen";
+            this.labelLauen.Size = new System.Drawing.Size(37, 13);
+            this.labelLauen.TabIndex = 0;
+            this.labelLauen.Text = "Lauen";
+            // 
+            // Lauen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelLauen);
+            this.Name = "Lauen";
+            this.Text = "Lauen";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Label labelLauen;
+    }
+}
