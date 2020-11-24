f1dcf2eddad86d16ca55a5a4fc64758343016f8c
nariolf111 <nariolf111@gmail.com>
2020-11-24 14:38:35 +0100

    Fenster FlorianDumke hinzugefügt

---
+++ HHBK_Chemicals_ERP_CS/FlorianDumke.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace HHBK_Chemicals_ERP_CS
+{
+    partial class FlorianDumke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(111, 75);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(72, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "FlorianDumke";
+            // 
+            // FlorianDumke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelName);
+            this.Name = "FlorianDumke";
+            this.Text = "FlorianDumke";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+    }
+}
