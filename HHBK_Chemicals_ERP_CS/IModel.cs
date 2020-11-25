907505f5d03bcfdf32bd004d89c19d85f6120fa2
KRIShark <cristofnagy@gmail.com>
2020-11-25 16:47:57 +0100

    Interfaces aded to Produktionsplan,Produktverwaltung,Rechnung

---
+++ HHBK_Chemicals_ERP_CS/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    interface IModel
+    {
+        int Create_Rechnung(string Num);
+        int Create_Rechnung();
+
+        string Table_Name { set; }
+    }
+}
