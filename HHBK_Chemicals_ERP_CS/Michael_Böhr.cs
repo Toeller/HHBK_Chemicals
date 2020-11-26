23dbd1e26ffbcb7669797a78880f89b3bb9e9a92
Michael Böhr <michael.boehr21@gmail.com>
2020-11-26 22:38:41 +0100

    Git Test Michael Böhr

---
+++ HHBK_Chemicals_ERP_CS/Michael_Böhr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class Michael_Böhr
+    {
+    }
+
+}
