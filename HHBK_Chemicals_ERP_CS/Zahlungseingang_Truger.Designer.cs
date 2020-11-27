794d8b62e7c86b3af277d71fc524ea981ec6c9a4
truger.leon <truger.leon@E211-18-12>
2020-11-27 12:11:46 +0100

    leon truger

--- HHBK_Chemicals_ERP_CS/Zahlungseingang_Truger.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Zahlungseingang_Truger.Designer.cs
@@ -30,38 +30,11 @@ namespace HHBK_Chemicals_ERP_CS
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(398, 287);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Zahlungseingang_Truger
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Zahlungseingang_Truger";
             this.Text = "Zahlungseingang_Truger";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Button button1;
     }
 }
