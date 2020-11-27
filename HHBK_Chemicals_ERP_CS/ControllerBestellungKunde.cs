68b89f3553f2dbd4e7b0b07a53e58f2680ba2d53
sakura-phoenix <celina.koeper@rapeedo.de>
2020-11-27 12:48:51 +0100

    veränderungen von IModel mit den Daten von  Zahlungseingang und von ControllerBestellungKunde

--- HHBK_Chemicals_ERP_CS/ControllerBestellungKunde.cs
+++ HHBK_Chemicals_ERP_CS/ControllerBestellungKunde.cs
@@ -9,8 +9,31 @@ namespace HHBK_Chemicals_ERP_CS
     class ControllerBestellungKunde
     {
         
-        public int Bestellungsnummer;
-        public int Menge;
+        public int Bestellungsnummer;                                                    //variable für die Bestellnummer
+        public int Menge;                                                                //variable für die Betstellungsmenge
+        public int Bestellpositionsnummer;                                               //variable für die Bestellungpositionsnummer
+
+
+        public void Speicher_Bestellungsnummer()                                         // Methode zur Speicherung  der Bestellungsnummer
+        {
+
+
+        }
+
+        public void Speichermenge()                                                      // Methode zur Speicherung der Menge
+        {
+
+
+        }
+
+
+        public void Speicher_Bestellpositionsnummer()                                     // Methode zur Speicherung Bestellpositionsnummer
+        {
+
+
+
+        }
+
 
     }
 }
--- HHBK_Chemicals_ERP_CS/IModel.cs
+++ HHBK_Chemicals_ERP_CS/IModel.cs
@@ -8,5 +8,16 @@ namespace HHBK_Chemicals_ERP_CS
 {
     interface IModel
     {
+        /*Zahlungseingang braucht in der Zukunft
+         *  den Zugang  auf die Daten der Datenbank Zahlungseingang*/
+
+        int Zahlungseingangsnummer { get; set; }                            // methode mit dem namen Zahlungseingangsnummer
+        string VerwendungszweckKdNr { get; set; }                           //methode mit dem namen VerwendungszweckKdNr
+        string VerwendungszweckBestNr { get; set; }                         //methode mit dem namen VerwendungszweckBestNr
+        string Datum { get; set; }                                          //methode mit dem namen Datum
+        string NameKontoinhaber { get; set; }                               //methode mit dem namen NameKontoinhaber
+        string IBAN { get; set; }                                           //methode mit dem namen IBAN 
+
+        void Ausgabe();                                                     // methode mit dem namen Ausgabe
     }
 }
