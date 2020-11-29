665f4142a7baf81bf423621cf1069a08e1585d41
Sevin <einpvpbro@gmail.com>
2020-11-29 12:12:17 +0100

    Controller_Bestellung(Kunde) und IModel_Zahlungseingang hinzugefügt

---
+++ HHBK_Chemicals_ERP_CS/Controller_Bestellung(Kunde).cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class Controller_Bestellung_Kunde_
+    {
+    
+        //warte noch auf nötigen Dateien :)
+    }
+}
+
---
+++ HHBK_Chemicals_ERP_CS/IModel_Zahlungseingang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    interface IModel_Zahlungseingang
+    
+        /* class Zahlungseingang
+        {
+            private int Zahlungseingang;
+
+            int ?.Zahlungseingang { get; set; }
+        } */
+
+}
