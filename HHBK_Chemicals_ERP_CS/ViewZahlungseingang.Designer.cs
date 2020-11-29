6a79fd275b514b91f3bcbcfb083d576cc6669956
Kevin Schenk <kev318668@gmail.com>
2020-11-29 23:05:17 +0100

    Interface (Fertigung,DB) / ZahlungseingangForm hinzugefügt

---
+++ HHBK_Chemicals_ERP_CS/ViewZahlungseingang.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace HHBK_Chemicals_ERP_CS
+{
+    partial class ViewZahlungseingang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelEingang = new System.Windows.Forms.Label();
+            this.labelZweckKNr = new System.Windows.Forms.Label();
+            this.labelZweckBNr = new System.Windows.Forms.Label();
+            this.labelDatum = new System.Windows.Forms.Label();
+            this.labelInhaber = new System.Windows.Forms.Label();
+            this.labelIban = new System.Windows.Forms.Label();
+            this.labelKundenNr = new System.Windows.Forms.Label();
+            this.labelKundenNr1 = new System.Windows.Forms.Label();
+            this.labelIban1 = new System.Windows.Forms.Label();
+            this.labelInhaber1 = new System.Windows.Forms.Label();
+            this.labelDatum1 = new System.Windows.Forms.Label();
+            this.labelZweckBNr1 = new System.Windows.Forms.Label();
+            this.labelZweckKNr1 = new System.Windows.Forms.Label();
+            this.labelEingang1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelEingang
+            // 
+            this.labelEingang.AutoSize = true;
+            this.labelEingang.Location = new System.Drawing.Point(12, 9);
+            this.labelEingang.Name = "labelEingang";
+            this.labelEingang.Size = new System.Drawing.Size(131, 13);
+            this.labelEingang.TabIndex = 0;
+            this.labelEingang.Text = "Zahlungseingangsnummer";
+            // 
+            // labelZweckKNr
+            // 
+            this.labelZweckKNr.AutoSize = true;
+            this.labelZweckKNr.Location = new System.Drawing.Point(13, 26);
+            this.labelZweckKNr.Name = "labelZweckKNr";
+            this.labelZweckKNr.Size = new System.Drawing.Size(127, 13);
+            this.labelZweckKNr.TabIndex = 1;
+            this.labelZweckKNr.Text = "VerwendungszweckKdNr";
+            this.labelZweckKNr.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelZweckBNr
+            // 
+            this.labelZweckBNr.AutoSize = true;
+            this.labelZweckBNr.Location = new System.Drawing.Point(13, 43);
+            this.labelZweckBNr.Name = "labelZweckBNr";
+            this.labelZweckBNr.Size = new System.Drawing.Size(135, 13);
+            this.labelZweckBNr.TabIndex = 2;
+            this.labelZweckBNr.Text = "VerwendungszweckBestNr";
+            // 
+            // labelDatum
+            // 
+            this.labelDatum.AutoSize = true;
+            this.labelDatum.Location = new System.Drawing.Point(13, 60);
+            this.labelDatum.Name = "labelDatum";
+            this.labelDatum.Size = new System.Drawing.Size(38, 13);
+            this.labelDatum.TabIndex = 3;
+            this.labelDatum.Text = "Datum";
+            // 
+            // labelInhaber
+            // 
+            this.labelInhaber.AutoSize = true;
+            this.labelInhaber.Location = new System.Drawing.Point(13, 77);
+            this.labelInhaber.Name = "labelInhaber";
+            this.labelInhaber.Size = new System.Drawing.Size(70, 13);
+            this.labelInhaber.TabIndex = 4;
+            this.labelInhaber.Text = "Kontoinhaber";
+            // 
+            // labelIban
+            // 
+            this.labelIban.AutoSize = true;
+            this.labelIban.Location = new System.Drawing.Point(13, 94);
+            this.labelIban.Name = "labelIban";
+            this.labelIban.Size = new System.Drawing.Size(32, 13);
+            this.labelIban.TabIndex = 5;
+            this.labelIban.Text = "IBAN";
+            // 
+            // labelKundenNr
+            // 
+            this.labelKundenNr.AutoSize = true;
+            this.labelKundenNr.Location = new System.Drawing.Point(13, 111);
+            this.labelKundenNr.Name = "labelKundenNr";
+            this.labelKundenNr.Size = new System.Drawing.Size(81, 13);
+            this.labelKundenNr.TabIndex = 6;
+            this.labelKundenNr.Text = "Kundennummer";
+            // 
+            // labelKundenNr1
+            // 
+            this.labelKundenNr1.AutoSize = true;
+            this.labelKundenNr1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelKundenNr1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelKundenNr1.Location = new System.Drawing.Point(168, 111);
+            this.labelKundenNr1.Name = "labelKundenNr1";
+            this.labelKundenNr1.Size = new System.Drawing.Size(81, 13);
+            this.labelKundenNr1.TabIndex = 7;
+            this.labelKundenNr1.Text = "Kundennummer";
+            // 
+            // labelIban1
+            // 
+            this.labelIban1.AutoSize = true;
+            this.labelIban1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelIban1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelIban1.Location = new System.Drawing.Point(168, 94);
+            this.labelIban1.Name = "labelIban1";
+            this.labelIban1.Size = new System.Drawing.Size(32, 13);
+            this.labelIban1.TabIndex = 8;
+            this.labelIban1.Text = "IBAN";
+            // 
+            // labelInhaber1
+            // 
+            this.labelInhaber1.AutoSize = true;
+            this.labelInhaber1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelInhaber1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelInhaber1.Location = new System.Drawing.Point(168, 77);
+            this.labelInhaber1.Name = "labelInhaber1";
+            this.labelInhaber1.Size = new System.Drawing.Size(70, 13);
+            this.labelInhaber1.TabIndex = 9;
+            this.labelInhaber1.Text = "Kontoinhaber";
+            // 
+            // labelDatum1
+            // 
+            this.labelDatum1.AutoSize = true;
+            this.labelDatum1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelDatum1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelDatum1.Location = new System.Drawing.Point(168, 60);
+            this.labelDatum1.Name = "labelDatum1";
+            this.labelDatum1.Size = new System.Drawing.Size(38, 13);
+            this.labelDatum1.TabIndex = 10;
+            this.labelDatum1.Text = "Datum";
+            // 
+            // labelZweckBNr1
+            // 
+            this.labelZweckBNr1.AutoSize = true;
+            this.labelZweckBNr1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelZweckBNr1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelZweckBNr1.Location = new System.Drawing.Point(168, 43);
+            this.labelZweckBNr1.Name = "labelZweckBNr1";
+            this.labelZweckBNr1.Size = new System.Drawing.Size(135, 13);
+            this.labelZweckBNr1.TabIndex = 11;
+            this.labelZweckBNr1.Text = "VerwendungszweckBestNr";
+            // 
+            // labelZweckKNr1
+            // 
+            this.labelZweckKNr1.AutoSize = true;
+            this.labelZweckKNr1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelZweckKNr1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelZweckKNr1.Location = new System.Drawing.Point(168, 26);
+            this.labelZweckKNr1.Name = "labelZweckKNr1";
+            this.labelZweckKNr1.Size = new System.Drawing.Size(127, 13);
+            this.labelZweckKNr1.TabIndex = 12;
+            this.labelZweckKNr1.Text = "VerwendungszweckKdNr";
+            // 
+            // labelEingang1
+            // 
+            this.labelEingang1.AutoSize = true;
+            this.labelEingang1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.labelEingang1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelEingang1.Location = new System.Drawing.Point(168, 9);
+            this.labelEingang1.Name = "labelEingang1";
+            this.labelEingang1.Size = new System.Drawing.Size(131, 13);
+            this.labelEingang1.TabIndex = 13;
+            this.labelEingang1.Text = "Zahlungseingangsnummer";
+            // 
+            // ViewZahlungseingang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(500, 141);
+            this.Controls.Add(this.labelEingang1);
+            this.Controls.Add(this.labelZweckKNr1);
+            this.Controls.Add(this.labelZweckBNr1);
+            this.Controls.Add(this.labelDatum1);
+            this.Controls.Add(this.labelInhaber1);
+            this.Controls.Add(this.labelIban1);
+            this.Controls.Add(this.labelKundenNr1);
+            this.Controls.Add(this.labelKundenNr);
+            this.Controls.Add(this.labelIban);
+            this.Controls.Add(this.labelInhaber);
+            this.Controls.Add(this.labelDatum);
+            this.Controls.Add(this.labelZweckBNr);
+            this.Controls.Add(this.labelZweckKNr);
+            this.Controls.Add(this.labelEingang);
+            this.Name = "ViewZahlungseingang";
+            this.Text = "ViewZahlungseingang";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelEingang;
+        private System.Windows.Forms.Label labelZweckKNr;
+        private System.Windows.Forms.Label labelZweckBNr;
+        private System.Windows.Forms.Label labelDatum;
+        private System.Windows.Forms.Label labelInhaber;
+        private System.Windows.Forms.Label labelIban;
+        private System.Windows.Forms.Label labelKundenNr;
+        private System.Windows.Forms.Label labelKundenNr1;
+        private System.Windows.Forms.Label labelIban1;
+        private System.Windows.Forms.Label labelInhaber1;
+        private System.Windows.Forms.Label labelDatum1;
+        private System.Windows.Forms.Label labelZweckBNr1;
+        private System.Windows.Forms.Label labelZweckKNr1;
+        private System.Windows.Forms.Label labelEingang1;
+    }
+}
