a79beaf6c1370a1862e559995273889a34ef6634
Christoph Töller <toeller@hhbk.de>
2020-12-01 16:22:27 +0100

    IModel Lieferposition

---
+++ HHBK_Chemicals_ERP_CS/IModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    interface IModel
+    {
+        //Kunde
+        Kunde GetKunde(int Kundennummer);
+
+        Lieferposition GetLieferposition(int idLieferposition);
+
+
+    }
+}
