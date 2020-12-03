88809d8f6e144d567b581aad2e372f9b7fdcb980
adam radi <adamradi03@gmail.com>
2020-12-03 20:46:08 +0100

    Um die Klassen Bestellposition und Lieferposition ergänzt

--- HHBK_Chemicals_ERP_CS/ViewFertigung.Designer.cs
+++ HHBK_Chemicals_ERP_CS/ViewFertigung.Designer.cs
@@ -28,12 +28,35 @@ namespace HHBK_Chemicals_ERP_CS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(123, 112);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+          
+            // 
+            // ViewFertigung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "ViewFertigung";
             this.Text = "ViewFertigung";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
