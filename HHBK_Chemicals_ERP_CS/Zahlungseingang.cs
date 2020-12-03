5670da422c9a18e2d6dc2d9de91d0b5061b72d3e
Töller HHBK on_premise <toeller@hhbk.de>
2020-12-03 09:05:20 +0100

    IModel begonnen, Klassen für Tabellen begonnen

---
+++ HHBK_Chemicals_ERP_CS/Zahlungseingang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    public class Zahlungseingang
+    {
+        private int zahlungseingangsnummer;
+        private string verwendungszweckKdNr;
+        private string verwendungszweckBestNr;
+        private DateTime datum;
+        private string nameKontoinhaber;
+        private string iBAN;
+        private int kundennummer;
+
+        public int Zahlungseingangsnummer 
+        { 
+            get => zahlungseingangsnummer; 
+            set => zahlungseingangsnummer = value; 
+        }
+        public string IBAN { 
+            get => iBAN; 
+            set => iBAN = value;
+        }
+    }
+}
