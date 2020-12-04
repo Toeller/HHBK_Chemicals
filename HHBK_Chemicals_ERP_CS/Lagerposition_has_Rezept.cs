5dfb698ce9b8059823ff785c9796da17f6965ce9
aybirdi.fatih <aybirdi.fatih@E211-18-08>
2020-12-04 12:20:10 +0100

    klasse Lagerposition_has_Rezept

--- HHBK_Chemicals_ERP_CS/Lagerposition_has_Rezept.cs
+++ HHBK_Chemicals_ERP_CS/Lagerposition_has_Rezept.cs
@@ -8,5 +8,15 @@ namespace HHBK_Chemicals_ERP_CS
 {
     class Lagerposition_has_Rezept
     {
+        private int lagerposition_Lagerpositionsnummer;
+        private int rezept_Rezeptnummer;
+        private int menge;
+        private String einheit;
+
+
+        public int Lagerposition_has_Lagerpositionsnummer { get; set; }
+        public int Rezept_Rezeptnummer { get; set; }
+        public int Menge { get; set; }
+        public string Einheit { get; set; }
     }
 }
