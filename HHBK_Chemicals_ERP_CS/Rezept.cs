0b70bc1576d70142c947a54dcf89c006e80a0e4b
aybirdi.fatih <aybirdi.fatih@E211-18-08>
2020-12-04 12:29:44 +0100

    class Rezept

---
+++ HHBK_Chemicals_ERP_CS/Rezept.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class Rezept
+    {
+        private int rezeptnummer;
+        private int herstellungsdauerMin;
+        private int produkt_Artikelnummer;
+
+        public int Rezeptnummer { get; set; }
+        public int HerstellungsdauerMin { get; set; }   
+        public int Produkt_Artikelnummer { get; set; }
+    }
+}
