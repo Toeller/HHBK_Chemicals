586ec19754f789383499e085316f69298ff5086b
KRIShark <cristofnagy@gmail.com>
2021-04-16 22:18:12 +0200

    ControllerBestellung und IControllerBestellung erstelt mit dei methoden: anlegen andern losen

---
+++ HHBK_Chemicals_ERP_CS/IController Bestellung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    interface IController_Bestellung
+    {
+        int Anlegen(Bestellung bestellung);
+        int Andern(Bestellung bestellung, Bestellung newBestellung);
+        int loschen(Bestellung bestellung);
+    }
+}
