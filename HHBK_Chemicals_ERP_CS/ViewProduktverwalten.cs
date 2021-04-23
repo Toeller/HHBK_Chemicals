f1050c50f9e5aa952786a05ab07293de2bf50ba6
Siam <siamlee.dang@gmail.com>
2021-04-23 11:55:12 +0200

    ViewProduktverwaltung

--- HHBK_Chemicals_ERP_CS/ViewProduktverwalten.cs
+++ HHBK_Chemicals_ERP_CS/ViewProduktverwalten.cs
@@ -9,6 +9,15 @@ namespace HHBK_Chemicals_ERP_CS
     public partial class ViewProduktverwalten
     {
         IViewProduktverwalten IViewProduktverwalten;
+
+        private int Produktnummer;
+        private string Produktname;
+        // private string 
+        // private string 
+        // private string 
+        // private string 
+        private int Datum;
+        // private string 
         public ViewProduktverwalten()
         { 
             
