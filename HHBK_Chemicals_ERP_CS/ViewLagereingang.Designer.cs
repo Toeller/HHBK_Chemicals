131d2009dede548d1d530591f6663ea8090929e6
Töller HHBK on_premise <toeller@hhbk.de>
2021-04-23 12:08:20 +0200

    Views ins Projekt genommen

--- HHBK_Chemicals_ERP_CS/ViewLagereingang.Designer.cs
+++ HHBK_Chemicals_ERP_CS/ViewLagereingang.Designer.cs
@@ -33,6 +33,7 @@
             this.Grundstoffname = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Grundstoffmenge = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Gundstoffeinheit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,11 +73,21 @@
             this.Gundstoffeinheit.HeaderText = "Gundstoffeinheit";
             this.Gundstoffeinheit.Name = "Gundstoffeinheit";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // ViewLagereingang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "ViewLagereingang";
             this.Text = "ViewLagereingang";
@@ -92,5 +103,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Grundstoffname;
         private System.Windows.Forms.DataGridViewTextBoxColumn Grundstoffmenge;
         private System.Windows.Forms.DataGridViewTextBoxColumn Gundstoffeinheit;
+        private System.Windows.Forms.Button button1;
     }
 }
