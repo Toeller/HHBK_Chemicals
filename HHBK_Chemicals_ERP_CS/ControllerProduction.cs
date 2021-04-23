d0854de4881b43007bf880d0379a39258a32293d
Ali El Malki <alielmalki16@hotmail.com>
2021-04-23 12:23:11 +0200

    -ControllerProduction Attribute hinzugefügt und gekapselt -IControllerProduction Vererbung

--- HHBK_Chemicals_ERP_CS/ControllerProduction.cs
+++ HHBK_Chemicals_ERP_CS/ControllerProduction.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace HHBK_Chemicals_ERP_CS
 {
-    class ControllerProduction
+    class ControllerProduction : IControllerProduction
     {
+        private int produktionspositionsnummer;
+        private int produkt_Artikelnummer;
+        private DateTime datumProduktionsfreigabe;
+        private DateTime datumProduktion;
+        private char nameProdkutionsfreigabe;
+        private char nameProduzent;
+        private int bestellposition_Bestellpositionsnummer;
+        private int bestellposition_Kunde_Kundennummer;
+
+        public int Produktionspositionsnummer { get; set; }
+        public int Produkt_Artikelnummer { get; set; }
+        public DateTime DatumProduktionsfreigabe { get; set; }
+        public DateTime DatumProduktion { get; set; }
+        public char NameProdkutionsfreigabe { get; set; }
+        public char NameProduzent { get; set; }
+        public int Bestellposition_Bestellpositionsnummer { get; set; }
+        public int Bestellposition_Kunde_Kundennummer { get; set; }
+
 
     }
 }
--- HHBK_Chemicals_ERP_CS/IControllerProduction.cs
+++ HHBK_Chemicals_ERP_CS/IControllerProduction.cs
@@ -8,6 +8,13 @@ namespace HHBK_Chemicals_ERP_CS
 {
     interface IControllerProduction
     {
-
+        int Produktionspositionsnummer { get; }
+        int Produkt_Artikelnummer { get; }
+        DateTime DatumProduktionsfreigabe { get; }
+        DateTime DatumProduktion { get; }
+        char NameProdkutionsfreigabe { get; }
+        char NameProduzent { get; }
+        int Bestellposition_Bestellpositionsnummer { get; set; }
+        int Bestellposition_Kunde_Kundennummer { get; set; }
     }
 }
