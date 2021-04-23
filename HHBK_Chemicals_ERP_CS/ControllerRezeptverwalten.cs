f99281156a8491461befab3abe74be50035fd9d5
claes.bastian <claes.bastian@E211-18-07>
2021-04-23 12:48:46 +0200

    yeet

---
+++ HHBK_Chemicals_ERP_CS/ControllerRezeptverwalten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class ControllerRezeptverwalten : IControllerRezeptverwalten
+    {
+       int Rezeptnummer;
+       int HerstellungsdauerMin;
+       int Produkt_Artikelnummer
+
+
+    }
+
+  
+}
