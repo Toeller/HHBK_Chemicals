9bd8067ba50bb7aefac86357b5d6de1f2179ef88
Mert Gözyuman <mert-goezy@hotmail.com>
2021-04-25 16:30:53 +0200

    View_Produktverwalten und IProduktverwalten hinzugefügt

---
+++ HHBK_Chemicals_ERP_CS/IProduktverwalten.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    interface IProduktverwalten
+    {
+        void eingeben();
+
+        void löschen();
+
+        void ändern();
+    }
+}
