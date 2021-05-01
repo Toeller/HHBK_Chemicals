8646ba7142eafb7ebdbac7d973180fd25b51277c
Aaron Schneider <aaron@dieschneiders.com>
2021-05-01 19:14:59 +0200

    Label geändert

--- HHBK_Chemicals_ERP_CS/IView Bestellung.cs
+++ HHBK_Chemicals_ERP_CS/IView Bestellung.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace View_Bestellung
+namespace HHBK_Chemicals_ERP_CS
 {
     interface IView_Bestellung
     {
+        int Anlegen();
+        int Louschen();
+        int Aondern();
+
+        IModel model1 {set; get;}
+        IController_Bestellung controler1 { set; get; } 
+
     }
+
 }
--- HHBK_Chemicals_ERP_CS/View Bestellung.cs
+++ HHBK_Chemicals_ERP_CS/View Bestellung.cs
@@ -8,18 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace View_Bestellung
+namespace HHBK_Chemicals_ERP_CS
 {
     public partial class Form1 : Form,IView_Bestellung
     {
-        private Button buttonBestellunganzeigen;
-        private Label labelProdunktdeteils;
-        private Label labelPreis;
-        private Button buttonBestellung;
+        private Button button1;
+        private Button button2;
+        private Label labelKundendaten;
+        private Label labelAdresse;
+        private Label labelArtikelnummer;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button3;
+
+        IModel IView_Bestellung.model1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        IController_Bestellung IView_Bestellung.controler1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int IView_Bestellung.Anlegen()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IView_Bestellung.Aondern()
+        {
+            throw new NotImplementedException();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,60 +45,125 @@ namespace View_Bestellung
 
         }
 
-        private void InitializeComponent()
+        /* private void InitializeComponent()
         {
-            this.buttonBestellung = new System.Windows.Forms.Button();
-            this.buttonBestellunganzeigen = new System.Windows.Forms.Button();
-            this.labelProdunktdeteils = new System.Windows.Forms.Label();
-            this.labelPreis = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelKundendaten = new System.Windows.Forms.Label();
+            this.labelAdresse = new System.Windows.Forms.Label();
+            this.labelArtikelnummer = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // buttonBestellung
+            // button1
             // 
-            this.buttonBestellung.Location = new System.Drawing.Point(12, 27);
-            this.buttonBestellung.Name = "buttonBestellung";
-            this.buttonBestellung.Size = new System.Drawing.Size(178, 49);
-            this.buttonBestellung.TabIndex = 0;
-            this.buttonBestellung.Text = "Produnkt Bestellung";
-            this.buttonBestellung.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(181, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 52);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Bestellen";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // buttonBestellunganzeigen
+            // button2
             // 
-            this.buttonBestellunganzeigen.Location = new System.Drawing.Point(12, 100);
-            this.buttonBestellunganzeigen.Name = "buttonBestellunganzeigen";
-            this.buttonBestellunganzeigen.Size = new System.Drawing.Size(178, 50);
-            this.buttonBestellunganzeigen.TabIndex = 1;
-            this.buttonBestellunganzeigen.Text = "Bestellung anzeigen";
-            this.buttonBestellunganzeigen.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(290, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Speichern";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // labelProdunktdeteils
+            // button3
             // 
-            this.labelProdunktdeteils.AutoSize = true;
-            this.labelProdunktdeteils.Location = new System.Drawing.Point(20, 190);
-            this.labelProdunktdeteils.Name = "labelProdunktdeteils";
-            this.labelProdunktdeteils.Size = new System.Drawing.Size(119, 20);
-            this.labelProdunktdeteils.TabIndex = 2;
-            this.labelProdunktdeteils.Text = "Produnktdeteils";
+            this.button3.Location = new System.Drawing.Point(69, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Löschen";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // labelPreis
+            // labelKundendaten
             // 
-            this.labelPreis.AutoSize = true;
-            this.labelPreis.Location = new System.Drawing.Point(24, 234);
-            this.labelPreis.Name = "labelPreis";
-            this.labelPreis.Size = new System.Drawing.Size(44, 20);
-            this.labelPreis.TabIndex = 3;
-            this.labelPreis.Text = "Preis";
+            this.labelKundendaten.AutoSize = true;
+            this.labelKundendaten.Location = new System.Drawing.Point(65, 61);
+            this.labelKundendaten.Name = "labelKundendaten";
+            this.labelKundendaten.Size = new System.Drawing.Size(105, 20);
+            this.labelKundendaten.TabIndex = 11;
+            this.labelKundendaten.Text = "Kundendaten";
+            // 
+            // labelAdresse
+            // 
+            this.labelAdresse.AutoSize = true;
+            this.labelAdresse.Location = new System.Drawing.Point(65, 106);
+            this.labelAdresse.Name = "labelAdresse";
+            this.labelAdresse.Size = new System.Drawing.Size(68, 20);
+            this.labelAdresse.TabIndex = 12;
+            this.labelAdresse.Text = "Adresse";
+            // 
+            // labelArtikelnummer
+            // 
+            this.labelArtikelnummer.AutoSize = true;
+            this.labelArtikelnummer.Location = new System.Drawing.Point(65, 149);
+            this.labelArtikelnummer.Name = "labelArtikelnummer";
+            this.labelArtikelnummer.Size = new System.Drawing.Size(111, 20);
+            this.labelArtikelnummer.TabIndex = 13;
+            this.labelArtikelnummer.Text = "Artikelnummer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(194, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(194, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 16;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(601, 309);
-            this.Controls.Add(this.labelPreis);
-            this.Controls.Add(this.labelProdunktdeteils);
-            this.Controls.Add(this.buttonBestellunganzeigen);
-            this.Controls.Add(this.buttonBestellung);
+            this.ClientSize = new System.Drawing.Size(471, 346);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelArtikelnummer);
+            this.Controls.Add(this.labelAdresse);
+            this.Controls.Add(this.labelKundendaten);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.Text = "Bestellung";
             this.ResumeLayout(false);
-            this.PerformLayout();
+            this.PerformLayout(); 
+
+        }
+        */
+        int IView_Bestellung.Louschen()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
 
         }
     }
