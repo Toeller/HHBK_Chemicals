7d1da291ad68e7d56a4db4e9b618c351dda29593
Noah Laptop <thecavmc@gmail.com>
2021-05-02 22:59:06 +0200

    added Controller / IController Rezeptverwaltung

---
+++ HHBK_Chemicals_ERP_CS/IControllerRezeptverwaltung.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHBK_Chemicals_ERP_CS
+{
+    class IControllerRezeptverwaltung
+    {
+        //Warten auf Pull Request @Azizcan
+    }
+}
