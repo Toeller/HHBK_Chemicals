92d08c28d6f3c5af1ae8f24c0d94caf8ef101834
KRIShark <cristofnagy@gmail.com>
2021-05-27 16:39:15 +0200

    test

---
+++ ControllerBestellungTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using HHBK_Chemicals_ERP_CS;
+
+//namespace ControllerBestellungTest
+namespace HHBK_Chemicals_ERP_CS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AndernTest()
+        {
+            IControllerBestellung v;
+        }
+    }
+}
