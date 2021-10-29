eed777a2bafff03f374b964d2377f66e5fb1a4b2
Töller HHBK on_premise <toeller@hhbk.de>
2021-10-29 12:59:33 +0200

    Test hinzugefügt

---
+++ TestProject1/ModelKundeAendern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Fuer den Test hinzugefügt:
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestProject1
+{
+
+    [TestClass]
+    class ModelKundeAendern
+    {
+        [TestMethod]
+    }
+}
---
+++ TestProject1/UnitTestModelKunde.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Fürs Testen:
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    class UnitTestModelKunde
+    {
+        [TestMethod]
+        public void getKunden_Void_ReturnsKundenListe()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+    }
+}
